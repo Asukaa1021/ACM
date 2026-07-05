60a93a91b5451ad06d5afdd256cce432acb0ef65
Asukaa1021 <zqyz2405536443@gmail.com>
2026-07-05 20:51:40 +0800

    Clean up: remove all source and executable files

--- P10908 [蓝桥杯 2024 国 B] 选数概率/P10908 [蓝桥杯 2024 国 B] 选数概率.cpp
+++
@@ -1,7 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-	cout << "55,94,56" << endl;
-}
--- P9231 [蓝桥杯 2023 省 A] 平方差/P9231 [蓝桥杯 2023 省 A] 平方差.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main()
-{
-	long long L,R;
-	cin >> L >> R;
-	cout << R - (R + 2) / 4 - L + 1 + (L + 1) / 4 << endl;
-}
